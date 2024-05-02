ffd64178a002d97190339f762b3d1bdc22536266
Paras Munoli <104249844+parasmunoli@users.noreply.github.com>
2024-05-02 10:16:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, request, jsonify
 import json
 import pickle
+from flask-cors import CORS
 
 app = Flask(__name__)
-
+CORS(app)
 
 with open('crop_prediction.pkl', 'rb') as model_file:
     crop_model = pickle.load(model_file)
@@ -18,28 +19,34 @@ with open('encoded_categories.json', 'r') as f:
 def fertilizer_predict():
     data = request.get_json()
 
-    temperature = data['Temperature']
-    humidity = data['Humidity']
-    moisture = data['Moisture']
-    soil_type = data['Soil Type']
-    crop_type = data['Crop Type']
-    nitrogen = data['Nitrogen']
-    potassium = data['Potassium']
-    phosphorous = data['Phosphorous']
-
-    encoded_soil_type = encoded_categories['Soil Type'].get(soil_type, None)
-    encoded_crop_type = encoded_categories['Crop Type'].get(crop_type, None)
-
-    if encoded_soil_type is None:
-        return jsonify({'error': f"'{soil_type}' not found in encoded categories for Soil Type."}), 400
-    if encoded_crop_type is None:
-        return jsonify({'error': f"'{crop_type}' not found in encoded categories for Crop Type."}), 400
-
-    prediction = fertilizer_model.predict([[temperature, humidity, moisture, encoded_soil_type, encoded_crop_type, nitrogen, potassium, phosphorous]])
-
-    predicted_fertilizer_name = prediction[0]
-
-    return jsonify({'predicted_fertilizer_name': predicted_fertilizer_name})
+    try:
+        temperature = data['Temperature']
+        humidity = data['Humidity']
+        moisture = data['Moisture']
+        soil_type = data['Soil Type']
+        crop_type = data['Crop Type']
+        nitrogen = data['Nitrogen']
+        potassium = data['Potassium']
+        phosphorous = data['Phosphorous']
+    
+        encoded_soil_type = encoded_categories['Soil Type'].get(soil_type, None)
+        encoded_crop_type = encoded_categories['Crop Type'].get(crop_type, None)
+    
+        if encoded_soil_type is None:
+            return jsonify({'error': f"'{soil_type}' not found in encoded categories for Soil Type."}), 400
+        if encoded_crop_type is None:
+            return jsonify({'error': f"'{crop_type}' not found in encoded categories for Crop Type."}), 400
+    
+        prediction = fertilizer_model.predict([[temperature, humidity, moisture, encoded_soil_type, encoded_crop_type, nitrogen, potassium, phosphorous]])
+    
+        predicted_fertilizer_name = prediction[0]
+    
+        return jsonify({'predicted_fertilizer_name': predicted_fertilizer_name})
+    
+    except KeyError as e:
+        return jsonify({'error': f'Missing key in JSON data: {str(e)}'}), 400
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 
 @app.route('/crop_predict', methods=['POST'])
